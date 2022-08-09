6e9e85afe8b7a4e244586f76b1b6f784d2396893
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-08-09 17:06:05 -0300

    possibilitando edição de restaurante

--- src/paginas/administracao/Restaurantes/FormularioRestaurante/index.tsx
+++ src/paginas/administracao/Restaurantes/FormularioRestaurante/index.tsx
@@ -1,18 +1,40 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../../interfaces/IRestaurante";
 
     export default function FormularioRestaurante(){
-    
-      const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+      const [nomeRestaurante, setNomeRestaurante] = useState('');
+
+      const parametros = useParams();
+
+      useEffect(()=>{
+        if(parametros.id){
+          axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+            .then(resposta=>{
+              setNomeRestaurante(resposta.data.nome)
+            })
+        }
+      },[parametros])
       
       const formSubmit = (evento: React.FormEvent<HTMLFormElement>)=>{
         evento.preventDefault();
-        axios.post('http://localhost:8000/api/v2/restaurantes/',{nome: nomeRestaurante})
-          .then(resposta=>{
-            console.log(resposta.data)
-            alert("restaurante cadastrado")
-          })
+
+        if(parametros.id){
+            axios.put<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`,{nome: nomeRestaurante})
+            .then(resposta=>{
+              console.log(resposta.data)
+              alert("Nome Alterado")
+            })
+        }else{
+          axios.post('http://localhost:8000/api/v2/restaurantes/',{nome: nomeRestaurante})
+            .then(resposta=>{
+              console.log(resposta.data)
+              alert("restaurante cadastrado")
+            })
+        }
       }  
 
     
