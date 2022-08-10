a66aa41c7dc81468270cd6a41761f5182c8daf2b
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-08-10 16:37:35 -0300

    criando página de adiministração de Pratos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import PaginaAdmin from './paginas/administracao/PaginaAdmin';
+import AdministracaoPratos from './paginas/administracao/Pratos';
+import FormularioPrato from './paginas/administracao/Pratos/FormularioPratos';
 import AdministracaoRestaurantes from './paginas/administracao/Restaurantes';
 import FormularioRestaurante from './paginas/administracao/Restaurantes/FormularioRestaurante';
 import Home from './paginas/Home';
@@ -16,6 +18,10 @@ function App() {
         <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
         <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
         <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+
+        <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
+        <Route path="pratos/:id" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
